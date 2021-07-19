b87e2f4326556a0e67366b4e58c77e7cc34cb478
22thking <70695171+22thking@users.noreply.github.com>
2021-07-19 22:50:21 +0900

    task 01.1
    code

--- task01/prac1.py
+++ task01/prac1.py
@@ -1,17 +1,22 @@
 import random
 
-random_number = random.randint(1,1000)
-print(random_number)
-while True:
-    try:
-        input_num = int(input("1~1000사이 숫자를 입력하세요:"))
-        if input_num == random_number:
-            print("정답")
-            break
-        elif input_num > random_number:
-            print("더 작은 수")
-        else:
-            print("더 큰 수")
-    except:
-        print("잘못된 입력")
 
+def solve():
+    random_number = random.randint(1, 1000)
+    print(random_number)
+    while True:
+        try:
+            input_num = int(input("1~1000사이 숫자를 입력하세요:"))
+            if input_num == random_number:
+                print("정답")
+                break
+            elif input_num > random_number:
+                print("더 작은 수")
+            else:
+                print("더 큰 수")
+        except:
+            print("잘못된 입력")
+
+
+if __name__ == "__main__":
+    assert solve()
