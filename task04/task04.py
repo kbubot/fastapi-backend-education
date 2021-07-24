d817318272527801553381675ecdf0bcf737df46
22thking <70695171+22thking@users.noreply.github.com>
2021-07-25 02:07:13 +0900

    task04
    먼저 함수에서 페이지 번호를 받은 후 그 사이트에 대한 정보를 받음 -> "https://www.bible.ac.kr/"+links[num]['href'] 이용해 링크를 저장한 뒤 링크에 들어가는 copy 변수에서 본문을 얻음

---
+++ task04/task04.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+from typing import List
+
+
+def get_notice_articles(page_num: int) -> List[List[str]]:
+    info: List[List[str]] = []
+    req = requests.get('https://www.bible.ac.kr/ko/life/notice/list/' + str(page_num))
+    html = req.content
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.select('span.title > a[href]')
+    titles = soup.select('li > span.title > a')
+    names = soup.select('li > span.name')
+    days = soup.select('li > span.reg_date')
+    num = 0
+    while num < 20:
+        info.append(["https://www.bible.ac.kr/"+links[num]['href'], titles[num].text, names[num+1].text, days[num+1].text])
+        contents = requests.get(info[num][0])
+        copy = BeautifulSoup(contents.content, 'html.parser')
+        main_text = copy.select_one('head > meta[name="description"]')['content']
+        info[num].append(main_text.strip())
+        num += 1
+    return info
+
+
+if __name__ == "__main__":
+    info: List[List[str]] = get_notice_articles(4)
+    for element in info:
+        print(element)
