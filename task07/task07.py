4e94ae159033043566d5f85046c75a0a8d4c4e2e
22thking <70695171+22thking@users.noreply.github.com>
2021-09-11 22:45:20 +0900

    Update task07.py

--- task07/task07.py
+++ task07/task07.py
@@ -1,4 +1,6 @@
-from typing import Dict, List, Optional
+from typing import Dict
+from typing import List
+from typing import Optional
 from dataclasses import dataclass
 from bs4 import BeautifulSoup
 import aiohttp
